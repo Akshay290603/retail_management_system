6bcb0e0153c84868ea9bab90ccc01b505f0ecbbe
Akshay290603 <akshaykawle2003@gmail.com>
2025-04-01 15:27:27 +0530

    cart and all changes

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
+import pyodbc
 from flask import Flask, render_template
 from flask_login import LoginManager, UserMixin, current_user, login_required
 
 app = Flask(__name__)
 app.secret_key = "1234"
+
+
+# SQL Server connection
+def get_db_connection():
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=MARCO\MSSQSERVER;'
+        'DATABASE=retail_management_system;'   
+        'Trusted_Connection=yes'  
+    )
+    return conn
+
+
 
 # # Initialize Flask-Login
 # login_manager = LoginManager()
@@ -20,7 +34,7 @@ app.secret_key = "1234"
 
 @app.route('/')
 def index():
-    return "Welcome to the Retail Management System!"
+    return render_template('index.html')
 
 @app.route('/layout')
 # @login_required
@@ -29,20 +43,53 @@ def layout():
 
 @app.route('/login')
 def login():
-    return "Login Page"
+    return render_template('login.html')
 
 @app.route('/register')
 def register():
-    return "Register Page"
+    return render_template('register.html')
 
 @app.route('/logout')
 def logout():
-    return "Logout Page"
+    return render_template('index.html')
 
 @app.route('/customer/home')
 @app.route('/customer/category')
 def category():
     return render_template('customer/category.html')
+
+@app.route('/customer/cart')
+def cart():
+    return render_template('customer/cart.html')
+
+@app.route('/retailer/home')
+@app.route('/retailer/current_orders')
+def current_orders():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    query = """
+    select od.order_id, p.product_name, od.quantity, od.unit_price, od.status, od.order_date
+    from OrderDetails as od
+    join Products as p on od.product_id = p.product_id
+    order by od.order_date desc
+    """
+    cursor.execute(query)
+    orders = [
+        {
+            'order_id': row[0],
+            'product_name': row[1],
+            'quantity': row[2],
+            'unit_price': row[3],
+            'status': row[4],
+            'order_date': row[5]
+        }
+        for row in cursor.fetchall()
+    ]
+
+    cursor.close()
+    conn.close()
+    return render_template('retailer/current_orders.html', orders=orders)
 
 if __name__ == "__main__":
     app.run(debug=True)
